ae49e48ddd276af81f7b2e20fef83515a165849d
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2026-02-17 12:48:21 -0500

    Initial Style Finder commit

---
+++ Style-Finder/src/main/java/CS431/Style_Finder/StyleFinderApplication.java
@@ -0,0 +1,13 @@
+package CS431.Style_Finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyleFinderApplication.class, args);
+	}
+
+}
---
+++ Style-Finder/src/test/java/CS431/Style_Finder/StyleFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package CS431.Style_Finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
