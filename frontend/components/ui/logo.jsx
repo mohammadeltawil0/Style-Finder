cb6a5709b49aeba689d6b620e05ddd58defb931b
fionughh <fiona.gumaroy@gmail.com>
2026-02-26 19:46:19 -0500

    fix: fixed error of not loading logo

--- frontend/components/ui/logo.jsx
+++ frontend/components/ui/logo.jsx
@@ -4,7 +4,7 @@ import { Image } from "react-native";
 export const Logo = () => {
   return (
     <Image
-      source={require("../../assets/images/logo.png")}
+      source={require("../../assets/images/custom-logo.png")}
       style={{ width: 20, height: 22, paddingVertical: 10 }}
     />
   );
