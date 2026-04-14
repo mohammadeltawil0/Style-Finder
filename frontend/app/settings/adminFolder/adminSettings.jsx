830d69ee384593dd860055b43b44806f7a3aec7c
fionughh <fiona.gumaroy@gmail.com>
2026-04-14 17:57:16 -0400

    fix: routing fix

--- frontend/app/settings/adminFolder/adminSettings.jsx
+++ frontend/app/settings/adminFolder/adminSettings.jsx
@@ -16,7 +16,7 @@ function AdminSettings() {
   const handleLogout = async () => {
     resetAnswers();
     await AsyncStorage.multiRemove(["username", "userId", "profileImageUrl", "role"]);
-    router.replace("/auth/logIn");
+    router.replace("/");
   };
 
   useEffect(() => {
